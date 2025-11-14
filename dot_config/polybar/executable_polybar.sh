fe3e6ee697396333cab45ae5c7b321c47fe6e4bf
Julius Breindl <jbr@condat.de>
2025-11-14 22:29:56 +0100

    updates

--- dot_config/polybar/executable_polybar.sh
+++ dot_config/polybar/executable_polybar.sh
@@ -6,8 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar &
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar &
 done
+
+
 
